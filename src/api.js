0632235673bfa069bfb965b0d408afde2ee14ad7
edAndrade <edmilisonwork@gmail.com>
2022-02-04 01:00:16 +0100

    configured dinamyc routes

--- src/api.js
+++ src/api.js
@@ -2,9 +2,15 @@ const Hapi = require('hapi')
 const Context = require('./db/strategies/base/contextStrategy')
 const MongoDB = require('./db/strategies/mongodb/mongodb')
 const HeroiSchema = require('./db/strategies/mongodb/schemas/heroisSchema')
+const HeroRoute = require('./routes/heroRoutes')
+
 const app = new Hapi.Server({
     port: 5000
 })
+
+function mapRoutes(instance, methods){
+    return methods.map(method => instance[method]())
+}
 
 async function main(){
 
@@ -12,13 +18,7 @@ async function main(){
     const context = new Context(new MongoDB(connection, HeroiSchema))
 
     app.route([
-        {
-            path: '/herois',
-            method: 'GET',
-            handler: (request, head) => {
-                return context.read()
-            }
-        }
+        ...mapRoutes(new HeroRoute(context), HeroRoute.methods())
     ])
 
     await app.start()
